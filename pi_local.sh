3c3e9742a1fb88892c89e288cb01e652909ac8c2
jason <dozejason@gmail.com>
2023-04-24 21:27:36 -0500

    Changed deploy_aws.sh to aws_install.sh.
    Add aws_login.sh to run with ssh -t to force psuedo-terminal allocation.
    
    Signed-off-by: jason <dozejason@gmail.com>

--- pi_local.sh
+++ pi_local.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # This script will log in the address of a raspberry-pi 400 ubuntu server, copy the files over the Pi, excute the script, and SSH into the server.
-PI_HOST=pi.attlocal.net
+PI_HOST=pi
 echo -e "\n==== Validate Pi server is running ====\n"
 while true
 do
-  if (  ping -c 1 $PI_HOST > /dev/null 2>&1 )
+  if ( ping -c 1 $PI_HOST > /dev/null 2>&1 )
   then
     echo -e "\n==== Server is running  ====\n"
     break
@@ -17,12 +17,12 @@ done
 
 # Use rsync to copy files to the Pi server
 echo -e "\n==== Copying files to Pi ====\n"
-rsync -av -e "ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_ed25519" --delete --exclude={'.git','.gitignore','commands.txt','README.md'} "$(pwd)" "$USER"@$PI_HOST:/home/"$USER"
+rsync -av -e "ssh -o StrictHostKeyChecking=no" --delete --exclude={'.git','.gitignore','commands.txt','README.md'} "$(pwd)" "$USER"@$PI_HOST:/home/"$USER"
 
 # Use SSH to execute commands on the Pi server
-echo -e "\n==== Executing install script ====\n"
-ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_ed25519 "$USER"@$PI_HOST 'cd cloud && bash deploy'
+echo -e "\n==== Executing aws_install script ====\n"
+ssh -o StrictHostKeyChecking=no "$USER"@$PI_HOST 'cd cloud && bash aws_install.sh'
 
 # SSH into Pi server
 echo -e "\n==== SSH into Pi ====\n"
-ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_ed25519 "$USER"@$PI_HOST
+ssh -t -o StrictHostKeyChecking=no "$USER"@$PI_HOST 'cd cloud && bash aws_login.sh'
